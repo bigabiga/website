814e17f0f60f9dc430a52f48e84af5428bc6fadb
Vadim Ogievetsky <vadimon@gmail.com>
2016-04-29 15:04:41 -0700

    vad work

---
+++ js/script.js
@@ -0,0 +1,135 @@
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'block')
+        e.style.display = 'none';
+    else
+        e.style.display = 'block';
+}
+
+$.fn.pressEnter = function(fn) {
+    return this.each(function() {
+        $(this).bind('enterPress', fn);
+        $(this).keyup(function(e){
+            if(e.keyCode == 13)
+            {
+                $(this).trigger("enterPress");
+            }
+        })
+    });
+};
+
+function replaceHashParam(paramName, paramValue) {
+    var hash = window.location.hash;
+    var pattern = new RegExp('\\b('+paramName+'=).*?(&|$)');
+    if(hash.search(pattern) >= 0){
+        hash = hash.replace(pattern,'$1' + paramValue + '$2');
+    } else {
+        hash += (hash ? '&' : '') + paramName + '=' + paramValue;
+    }
+    window.location.hash = hash;
+}
+
+function getParameterByName(name) {
+    var match = new RegExp('[#&]' + name + '=([^&]*)').exec(window.location.hash);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+function updateState() {
+    function ascending(a, b) {
+        return (a < b) ? -1 : (a > b) ? 1 : 0;
+    }
+
+    var $thumbnail = $('.thumbnail');
+
+    var $categorySearch = $('#categorySearch');
+    var $removeSearch = $('#removeSearch');
+
+    var search = getParameterByName('search');
+    $categorySearch.val(search);
+    if (search) {
+        $categorySearch.addClass("full")
+        $removeSearch.show();
+        var query = search.toLowerCase();
+        $thumbnail.each(function() {
+            var $this = $(this);
+            var title = $this.find('.title a').text();
+            if (title.toLowerCase().indexOf(query) === -1) {
+                $this.hide();
+            } else {
+                $this.show();
+            }
+        });
+    } else {
+        $categorySearch.removeClass("full");
+        $removeSearch.hide();
+        $thumbnail.each(function() {
+            var $this = $(this);
+            $this.show();
+        });
+    }
+
+    var sort = getParameterByName('sort');
+    $("#sortBy").text(sort);
+    if (sort) {
+        switch (sort.toLowerCase()) {
+            case 'alphabetically':
+                $thumbnail.sortElements(function(a, b) {
+                    var $a = $(a).find('.title a');
+                    var $b = $(b).find('.title a');
+                    var contentA = $a.text();
+                    var contentB = $b.text();
+                    return ascending(contentA, contentB);
+                });
+                break;
+
+            case 'year':
+                $thumbnail.sortElements(function(a, b) {
+                    var $a = $(a);
+                    var $b = $(b);
+                    var contentA = parseInt($a.attr('data-year'));
+                    var contentB = parseInt($b.attr('data-year'));
+                    return ascending(contentA, contentB);
+                });
+                break;
+
+            case 'recommended':
+                $thumbnail.sortElements(function(a, b) {
+                    var $a = $(a);
+                    var $b = $(b);
+                    var contentA = parseInt($a.attr('data-recommended'));
+                    var contentB = parseInt($b.attr('data-recommended'));
+                    return -ascending(contentA, contentB);
+                });
+                break;
+        }
+    }
+}
+
+window.onhashchange = updateState;
+
+function updateSort(sortValue) {
+    console.log('updateSort', sortValue);
+    replaceHashParam('sort', sortValue);
+}
+
+function updateSearch(searchValue) {
+    replaceHashParam('search', searchValue);
+}
+
+$(function() {
+    updateState();
+
+    var $categorySearch = $('#categorySearch');
+
+    //show the search remover only when there is something in the input:
+    $categorySearch.on('input', function() {
+        replaceHashParam('search', $categorySearch.val());
+        updateRemover();
+    });
+
+    var $globalSearch = $('#global-search');
+    $globalSearch.pressEnter(function() {
+        var url = window.location.host + "/search/?sort=year&search=" + $globalSearch.val();
+        window.location.url = url;
+    });
+});
